6839e9fbac6a0fc1032b02e2b1656b5402f7c23a
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-27 20:41:12 +0330

    starting nav element

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navbg: "#171717",
+      },
+    },
+  },
+  plugins: [],
+};
